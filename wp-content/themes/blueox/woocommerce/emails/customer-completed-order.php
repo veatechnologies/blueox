472390a8b66404868c5a96529966b690251a0dd6
veadev <ayushi.sharma@kelltontech.org>
2020-02-05 19:14:00 +0530

    added on master

--- wp-content/themes/blueox/woocommerce/emails/customer-completed-order.php
+++ wp-content/themes/blueox/woocommerce/emails/customer-completed-order.php
@@ -57,6 +57,57 @@ do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 	<td>Delivered</td>
 		</tbody>
 		</table>
+		
+		
+		<table class="td" cellspacing="0" cellpadding="6" style="width: 100%; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif;" border="1">
+		<thead>
+			<!--<tr>
+				<th class="td" scope="col" style="text-align:<?php //echo esc_attr( $text_align ); ?>;"><?php //esc_html_e( 'Product', 'woocommerce' ); ?></th>
+				<th class="td" scope="col" style="text-align:<?php //echo esc_attr( $text_align ); ?>;"><?php //esc_html_e( 'Quantity', 'woocommerce' ); ?></th>
+				<th class="td" scope="col" style="text-align:<?php //echo esc_attr( $text_align ); ?>;"><?php //esc_html_e( 'Price', 'woocommerce' ); ?></th>
+			</tr> -->
+		</thead>
+		<tbody>
+			<?php
+			echo wc_get_email_order_items( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+				$order,
+				array(
+					'show_sku'      => $sent_to_admin,
+					'show_image'    => false,
+					'image_size'    => array( 32, 32 ),
+					'plain_text'    => $plain_text,
+					'sent_to_admin' => $sent_to_admin,
+				)
+			);
+			?>
+		</tbody>
+		<tfoot>
+			<?php
+			$item_totals = $order->get_order_item_totals();
+
+			if ( $item_totals ) {
+				$i = 0;
+				foreach ( $item_totals as $total ) {
+					$i++;
+					?>
+					<tr>
+						<th class="td" scope="row" colspan="2" style="text-align:<?php echo esc_attr( $text_align ); ?>; <?php echo ( 1 === $i ) ? 'border-top-width: 4px;' : ''; ?>"><?php echo wp_kses_post( $total['label'] ); ?></th>
+						<td class="td" style="text-align:<?php echo esc_attr( $text_align ); ?>; <?php echo ( 1 === $i ) ? 'border-top-width: 4px;' : ''; ?>"><?php echo wp_kses_post( $total['value'] ); ?></td>
+					</tr>
+					<?php
+				}
+			}
+			if ( $order->get_customer_note() ) {
+				?>
+				<tr>
+					<th class="td" scope="row" colspan="2" style="text-align:<?php echo esc_attr( $text_align ); ?>;"><?php esc_html_e( 'Note:', 'woocommerce' ); ?></th>
+					<td class="td" style="text-align:<?php echo esc_attr( $text_align ); ?>;"><?php echo wp_kses_post( nl2br( wptexturize( $order->get_customer_note() ) ) ); ?></td>
+				</tr>
+				<?php
+			}
+			?>
+		</tfoot>
+	</table>
 <?php
 
 /*
