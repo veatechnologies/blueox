d6afd1ca4739634529b77ee828360a24e2fbb6d0
veadev <ayushi.sharma@kelltontech.org>
2020-02-06 13:27:31 +0530

    added on local

--- wp-content/themes/blueox/woocommerce/emails/customer-completed-order.php
+++ wp-content/themes/blueox/woocommerce/emails/customer-completed-order.php
@@ -61,8 +61,6 @@ do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 			);
 			?>
 			
-			
-			
 			<?php
 			$item_totals = $order->get_order_item_totals();
 
@@ -71,21 +69,22 @@ do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 				foreach ( $item_totals as $total ) {
 					$i++;
 					?>
-					<tr>
-						<th class="td" scope="row" colspan="2" style="text-align:<?php echo esc_attr( $text_align ); ?>; <?php echo ( 1 === $i ) ? 'border-top-width: 4px;' : ''; ?>"><?php echo wp_kses_post( $total['label'] ); ?></th>
-						<td class="td" style="text-align:<?php echo esc_attr( $text_align ); ?>; <?php echo ( 1 === $i ) ? 'border-top-width: 4px;' : ''; ?>"><?php echo wp_kses_post( $total['value'] ); ?></td>
-					</tr>
+					
+					<p class="product-name"> <?php echo wp_kses_post( $total['label'] ); ?> </p>
+						
+					<p class="product price"><?php echo wp_kses_post( $total['value'] ); ?></p>
+				
 					<?php
 				}
 			}
-			if ( $order->get_customer_note() ) {
+			/*if ( $order->get_customer_note() ) {
 				?>
 				<tr>
 					<th class="td" scope="row" colspan="2" style="text-align:<?php echo esc_attr( $text_align ); ?>;"><?php esc_html_e( 'Note:', 'woocommerce' ); ?></th>
 					<td class="td" style="text-align:<?php echo esc_attr( $text_align ); ?>;"><?php echo wp_kses_post( nl2br( wptexturize( $order->get_customer_note() ) ) ); ?></td>
 				</tr>
 				<?php
-			}
+			}*/
 			?>
 
 
