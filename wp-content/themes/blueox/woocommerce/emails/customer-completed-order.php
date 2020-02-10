bf3fbbeb3dc52ab0739a776b63acb2dd3860c376
veadev <ayushi.sharma@kelltontech.org>
2020-02-10 15:29:22 +0530

    add on local

--- wp-content/themes/blueox/woocommerce/emails/customer-completed-order.php
+++ wp-content/themes/blueox/woocommerce/emails/customer-completed-order.php
@@ -91,7 +91,7 @@ do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 /*
  * @hooked WC_Emails::order_meta() Shows order meta data.
  */
-do_action( 'woocommerce_email_order_meta', $order, $sent_to_admin, $plain_text, $email );
+//do_action( 'woocommerce_email_order_meta', $order, $sent_to_admin, $plain_text, $email );
 
 /*
  * @hooked WC_Emails::customer_details() Shows customer details
