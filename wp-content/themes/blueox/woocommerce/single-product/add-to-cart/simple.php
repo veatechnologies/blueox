65e75ed61bdd2633bf78611c16218df382918cd1
veadev <ayushi.sharma@kelltontech.org>
2020-02-13 18:58:58 +0530

    add file simple.php

--- wp-content/themes/blueox/woocommerce/single-product/add-to-cart/simple.php
+++ wp-content/themes/blueox/woocommerce/single-product/add-to-cart/simple.php
@@ -43,9 +43,10 @@ if ( $product->is_in_stock() ) : ?>
 
 		do_action( 'woocommerce_after_add_to_cart_quantity' );
 		?>
-
+		<div class="product-proce-section">
 		<button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="single_add_to_cart_button button alt"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
 		<a class="dealer-link" href="<?php echo  get_site_url()?>/dealer-locator/">Find a Dealer</a>
+		</div>
 		<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
 	</form>
 	
