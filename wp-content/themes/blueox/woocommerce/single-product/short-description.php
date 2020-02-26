c9adf2150bf58356aed9a79f24137ca251093192
veadev <ayushi.sharma@kelltontech.org>
2020-02-26 17:02:40 +0530

    added short description file

--- wp-content/themes/blueox/woocommerce/single-product/short-description.php
+++ wp-content/themes/blueox/woocommerce/single-product/short-description.php
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-global $post;
+global $post, $product;
 
 $short_description = apply_filters( 'woocommerce_short_description', $post->post_excerpt );
 
@@ -29,8 +29,35 @@ if ( ! $short_description ) {
 
 ?>
 <div class="woocommerce-product-details__short-description">
-	<?php echo $short_description; // WPCS: XSS ok.
+<?php echo $short_description; // WPCS: XSS ok.
 
+// For Attributes
+$class = $product->get_attribute( 'class' );
+$baseplate_year = $product->get_attribute( 'baseplate_year' );
+$make = $product->get_attribute( 'make' );
+$material = $product->get_attribute( 'material' );
+$models = $product->get_attribute( 'models' );
+$weight = $product->get_attribute( 'weight' );
+
+if(!empty($class)){echo $class."<br>";}
+if(!empty($baseplate_year)){echo $baseplate_year."<br>";}
+if(!empty($make)){echo $make."<br>";} 
+if(!empty($material)){echo $material."<br>";} 
+if(!empty($models)){ echo $models."<br>"; }
+if(!empty($weight)){ echo $weight; }
+
+//For Install instruction
+
+$productID = $product->get_id();
+$install_instruction_pdf= get_field('install_instruction', $productID);
+$warrenty= get_field('warrenty', $productID);
+if(!empty($install_instruction_pdf)){ ?>
+	<a class="install-instruction-link" target="_blank" href="<?php echo $install_instruction_pdf; ?>">installation instruction</a>
+	<?php }
+if(!empty($warrenty)){ ?>
+	<a class="warrenty-link" target="_blank" href="<?php echo $warrenty; ?>">warrenty</a>
+	<?php }
 	?>
+
 </div>
 
