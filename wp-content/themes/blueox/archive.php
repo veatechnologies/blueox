1d66a2d41be2923012b5dff997632e9cc9afa277
veadev <veatechnologies.com>
2020-04-06 04:58:15 -0700

    add file on local

--- wp-content/themes/blueox/archive.php
+++ wp-content/themes/blueox/archive.php
@@ -28,7 +28,7 @@ get_header(); ?>
 					<?php
 					if ( have_posts() ) : ?>
 
-						<header class="page-header mt-3r">
+						<header class="page-header mt-3r test">
 							<?php
 								the_archive_title( '<h1 class="page-title">', '</h1>' );
 								the_archive_description( '<div class="archive-description text-muted">', '</div>' );
