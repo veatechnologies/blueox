4060db5365ff240b199b5173a925c023f5b633b2
veadev <veatechnologies.com>
2020-04-06 04:26:56 -0700

    add files

--- wp-content/themes/blueox/template-parts/content-search.php
+++ wp-content/themes/blueox/template-parts/content-search.php
@@ -10,8 +10,8 @@
 ?>
 
 <div class="container">
-	<div class="col-md-4">
-<article id="post-<?php the_ID(); ?>test" <?php post_class( 'card mt-3r' ); ?>>
+	<div class="col-md-4" <?php post_class( 'card mt-3r' ); ?> id="post-<?php the_ID(); ?>test">
+<!--<article> -->
 	<div class="card-body">
 		<header class="entry-header">
 			<?php the_title( sprintf( '<h2 class="entry-title card-title"><a href="%s" rel="bookmark" class="text-dark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
@@ -36,6 +36,6 @@
 			<?php wp_bootstrap_4_entry_footer(); ?>
 		</footer><!-- .entry-footer -->
 	<?php endif; ?>
-</article><!-- #post-<?php the_ID(); ?> -->
+<!--</article>--><!-- #post-<?php the_ID(); ?> -->
 </div>
 </div>
